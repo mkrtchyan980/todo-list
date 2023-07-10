f63a8d6ab3f9e6d17d91a8beeddd71102163d76c
Sveta Mkrtchyan <sveta.mkrtchyan.11@gmail.com>
2023-07-10 16:15:26 +0300

    todo list

--- src/component/TodoFooter.jsx
+++ src/component/TodoFooter.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 
-function TodoFooter({todos,onClearCompleted}) {
-  const completedSize = todos.filter((todo) => todo.isCompleted).length
-  return (
-    <div className='todoFooter'>
-        <span>{completedSize}/{todos.length}is Completed</span>
-        <button onClick={onClearCompleted}>Clear Completed</button>
-    </div>
-  )
+function TodoFooter({ todos, onClearCompleted }) {
+    const completedSize = todos.filter((todo) => todo.isCompleted).length;
+    return (
+        <div className="todoFooter">
+            <span>
+                {completedSize}/{todos.length}is Completed
+            </span>
+            <button onClick={onClearCompleted}>Clear Completed</button>
+        </div>
+    );
 }
 
-export default TodoFooter
+export default TodoFooter;
