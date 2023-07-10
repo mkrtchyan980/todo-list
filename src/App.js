774f7eb6a9fb01d5e34091a92a9e3ea0488909fb
Sveta Mkrtchyan <sveta.mkrtchyan.11@gmail.com>
2023-07-10 16:04:03 +0300

    todo list

--- src/App.js
+++ src/App.js
@@ -1,65 +1,95 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "../src/index.css";
 import TodoList from "./component/TodoList";
 import TodoForm from "./component/TodoForm";
 import TodoFooter from "./component/TodoFooter";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: Math.random(),
-      text: "React",
-      isCompleted: false,
-    },
-    {
-      id: Math.random(),
-      text: "JavaScript",
-      isCompleted: false,
-    },
-    {
-      id: Math.random(),
-      text: "Html",
-      isCompleted: false,
-    },
-  ]);
-  return (
-    <div className="child">
-      <TodoForm
-        onAdd={(text) => {
-          setTodos([
-            ...todos,
+function reduce(state, action) {
+    if (action.type === "add") {
+        return [
+            ...state,
             {
-              id: Math.random(),
-              text: text,
-              isCompleted: false,
+                id: Math.random(),
+                text: action.payload.text,
+                isCompleted: false,
             },
-          ]);
-        }}
-      />
-      <TodoList
-        todos={todos}
-        onDelate={(todo) => {
-          setTodos(todos.filter((t) => t.id !== todo.id));
-        }}
-        onChange={(newTodo) => {
-          setTodos(
-            todos.map((todo) => {
-              if (newTodo.id === todo.id) {
-                return newTodo;
-              }
-              return todo;
-            })
-          );
-        }}
-      />
-      <TodoFooter
-        todos={todos}
-        onClearCompleted={() => {
-          setTodos(todos.filter((t) => !t.isCompleted));
-        }}
-      />
-    </div>
-  );
+        ];
+    } else if (action.type === "delete") {
+        return state.filter((todo) => todo.id !== action.payload.id);
+    } else if (action.type === "onchange") {
+        return state.map((todo) => {
+            if (action.payload.newTodo.id === todo.id) {
+                return action.payload.newTodo;
+            }
+            return todo;
+        });
+    } else if (action.type === "clearCompleted") {
+        return state.filter((t) => !t.isCompleted);
+    }
+}
+
+function App() {
+    const [todos, dispatch] = useReducer(reduce, [
+        {
+            id: Math.random(),
+            text: "React",
+            isCompleted: false,
+        },
+        {
+            id: Math.random(),
+            text: "JavaScript",
+            isCompleted: false,
+        },
+        {
+            id: Math.random(),
+            text: "Html",
+            isCompleted: false,
+        },
+    ]);
+    return (
+        <div className="child">
+            <TodoForm
+                onAdd={(text) => {
+                    dispatch({
+                        type: "add",
+                        payload: {
+                            text: text,
+                        },
+                    });
+                }}
+            />
+            <TodoList
+                todos={todos}
+                onDelate={(todo) => {
+                    dispatch({
+                        type: "delete",
+                        payload: {
+                            id: todo.id,
+                        },
+                    });
+                }}
+                onChange={(newTodo) => {
+                    dispatch({
+                        type: "onchange",
+                        payload: {
+                            newTodo: newTodo,
+                        },
+                    });
+                }}
+            />
+            <TodoFooter
+                todos={todos}
+                onClearCompleted={(todo) => {
+                    dispatch({
+                        type: "clearCompleted",
+                        payload: {
+                            todo: todo,
+                        },
+                    });
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
